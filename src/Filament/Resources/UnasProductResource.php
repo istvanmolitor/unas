38cf8b2e9affa85338ccbe5877ca0391286190c0
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-11-27 22:06:33 +0100

    permission bug

--- src/Filament/Resources/UnasProductResource.php
+++ src/Filament/Resources/UnasProductResource.php
@@ -29,7 +29,7 @@ class UnasProductResource extends Resource
 
     public static function canAccess(): bool
     {
-        return Gate::allows('acl', 'unas') && request()->has('warehouse_id');
+        return Gate::allows('acl', 'unas');
     }
 
     public static function form(Schema $schema): Schema
