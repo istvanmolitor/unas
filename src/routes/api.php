8c2494c4bcfcf78613549bf6ceab2a67b495d494
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-05-28 20:33:50 +0200

    api

--- src/routes/api.php
+++ src/routes/api.php
@@ -10,8 +10,15 @@ use Molitor\Unas\Http\Controllers\Api\UnasShopController;
 
 Route::prefix('api/unas')->middleware(['api', 'auth:sanctum'])->group(function (): void {
     Route::get('shops/options', [UnasShopController::class, 'options']);
-    Route::apiResource('shops', UnasShopController::class);
-    Route::apiResource('products', UnasProductController::class)->only(['index', 'show', 'destroy']);
-    Route::apiResource('categories', UnasProductCategoryController::class)->only(['index', 'show', 'destroy']);
-    Route::apiResource('parameters', UnasProductParameterController::class)->only(['index', 'show', 'destroy']);
+    Route::apiResource('shops', UnasShopController::class)
+        ->parameters(['shops' => 'unasShop']);
+    Route::apiResource('products', UnasProductController::class)
+        ->parameters(['products' => 'unasProduct'])
+        ->only(['index', 'show', 'destroy']);
+    Route::apiResource('categories', UnasProductCategoryController::class)
+        ->parameters(['categories' => 'unasProductCategory'])
+        ->only(['index', 'show', 'destroy']);
+    Route::apiResource('parameters', UnasProductParameterController::class)
+        ->parameters(['parameters' => 'unasProductParameter'])
+        ->only(['index', 'show', 'destroy']);
 });
